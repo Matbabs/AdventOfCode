e647161cc4ed7d4b90deb8516b58526b246bf0bf
matisse babonneau <matisse.babonneau@orange.com>
2022-12-08 14:40:19 +0100

    f

--- 2022/8/visualization.go
+++ 2022/8/visualization.go
@@ -26,27 +26,32 @@ func main() {
 			if !(i == 0 || j == 0 || i == len(_map)-1 || j == len(_map[i])-1) {
 				score := 0
 				t, r, b, l := 0, 0, 0, 0
+				tv, tr, tb, tl := true, true, true, true
 				for d := i - 1; d >= 0; d-- {
 					t++
 					if _map[d][j] >= _map[i][j] {
+						tv = false
 						break
 					}
 				}
 				for d := j + 1; d < len(_map[i]); d++ {
 					r++
 					if _map[i][d] >= _map[i][j] {
+						tr = false
 						break
 					}
 				}
 				for d := i + 1; d < len(_map); d++ {
 					b++
 					if _map[d][j] >= _map[i][j] {
+						tb = false
 						break
 					}
 				}
 				for d := j - 1; d >= 0; d-- {
 					l++
 					if _map[i][d] >= _map[i][j] {
+						tl = false
 						break
 					}
 				}
@@ -54,17 +59,23 @@ func main() {
 				if score > panoramic {
 					panoramic = score
 				}
-				if score > 75 {
-					fmt.Print(GREEN, "0")
-				} else if score > 50 {
-					fmt.Print(GREEN, "O")
-				} else if score > 25 {
-					fmt.Print(GREEN, "o")
+				if tv || tr || tb || tl {
+					if score > 100 {
+						fmt.Print(GREEN, "@")
+					} else if score > 75 {
+						fmt.Print(GREEN, "0")
+					} else if score > 50 {
+						fmt.Print(GREEN, "O")
+					} else if score > 25 {
+						fmt.Print(GREEN, "o")
+					} else {
+						fmt.Print(GREEN, "°")
+					}
 				} else {
-					fmt.Print(GREEN, ".")
+					fmt.Print(".")
 				}
 			} else {
-				fmt.Print(GREEN, ".")
+				fmt.Print(GREEN, "°")
 			}
 		}
 		fmt.Println()
