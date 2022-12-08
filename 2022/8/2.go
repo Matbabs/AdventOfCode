56827b0340eea4510ae4ace445b3bd7861e55686
Matisse BABONNEAU <dnbh7461@EB-L-OR6221205.ad-subs.w2k.francetelecom.fr>
2022-12-08 08:52:05 +0100

    fx day 8

--- 2022/8/2.go
+++ 2022/8/2.go
@@ -23,47 +23,34 @@ func main() {
 		for j := 0; j < len(_map[i]); j++ {
 			if !(i == 0 || j == 0 || i == len(_map)-1 || j == len(_map[i])-1) {
 				score := 0
-				t := true
-				t_s := 0
+				t, r, b, l := 0, 0, 0, 0
 				for d := i - 1; d >= 0; d-- {
-					t_s++
+					t++
 					if _map[d][j] >= _map[i][j] {
-						t = false
 						break
 					}
 				}
-				r := true
-				r_s := 0
 				for d := j + 1; d < len(_map[i]); d++ {
-					r_s++
+					r++
 					if _map[i][d] >= _map[i][j] {
-						r = false
 						break
 					}
 				}
-				b := true
-				b_s := 0
 				for d := i + 1; d < len(_map); d++ {
-					b_s++
+					b++
 					if _map[d][j] >= _map[i][j] {
-						b = false
 						break
 					}
 				}
-				l := true
-				l_s := 0
 				for d := j - 1; d >= 0; d-- {
-					l_s++
+					l++
 					if _map[i][d] >= _map[i][j] {
-						l = false
 						break
 					}
 				}
-				if t || r || b || l {
-					score = t_s * r_s * b_s * l_s
-					if score > panoramic {
-						panoramic = score
-					}
+				score = t * r * b * l
+				if score > panoramic {
+					panoramic = score
 				}
 			}
 		}
