27fc7ddcff45069f44ffb8d466c1cd8a74e56025
Matisse BABONNEAU <dnbh7461@EB-L-OR6221205.ad-subs.w2k.francetelecom.fr>
2022-12-11 12:59:43 +0100

    day 11

---
+++ 2022/11/1.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"os/exec"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const TURNS = 20
+
+type Monkey struct {
+	items []int
+	ops   string
+	test  int
+	true  int
+	false int
+}
+
+func getInt(line string) int {
+	split := strings.Split(line, " ")
+	v, _ := strconv.Atoi(split[len(split)-1])
+	return v
+}
+
+func main() {
+	inspections := make([]int, 0)
+	monkeys := make([]Monkey, 0)
+	monkey := Monkey{items: []int{}}
+	num_line := 0
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		switch num_line {
+		case 1:
+			re := regexp.MustCompile(`\d+`)
+			for _, n := range re.FindAllString(line, -1) {
+				v, _ := strconv.Atoi(n)
+				monkey.items = append(monkey.items, v)
+			}
+		case 2:
+			monkey.ops = strings.Split(line, "=")[1]
+		case 3:
+			monkey.test = getInt(line)
+		case 4:
+			monkey.true = getInt(line)
+		case 5:
+			monkey.false = getInt(line)
+			monkeys = append(monkeys, monkey)
+			inspections = append(inspections, 0)
+			monkey = Monkey{items: []int{}}
+			num_line = -2
+		}
+		num_line++
+	}
+	for i := 0; i < TURNS; i++ {
+		for j := range monkeys {
+			for len(monkeys[j].items) > 0 {
+				item := monkeys[j].items[0]
+				monkeys[j].items = monkeys[j].items[1:]
+				exp := strings.ReplaceAll(monkeys[j].ops, "old", strconv.Itoa(item))
+				cmd, _ := exec.Command("bash", "-c", fmt.Sprintf("echo $((%s))", exp)).Output()
+				lvl, _ := strconv.Atoi(string(cmd[:len(cmd)-1]))
+				lvl = int(math.Floor(float64(lvl / 3)))
+				if lvl%monkeys[j].test == 0 {
+					monkeys[monkeys[j].true].items = append(monkeys[monkeys[j].true].items, lvl)
+				} else {
+					monkeys[monkeys[j].false].items = append(monkeys[monkeys[j].false].items, lvl)
+				}
+				inspections[j]++
+			}
+		}
+	}
+	sort.Ints(inspections)
+	fmt.Println(inspections[len(inspections)-1] * inspections[len(inspections)-2])
+}
