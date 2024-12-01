6bdc325b4a95a7509f6d71b20ebe5899adacf7f3
BABONNEAU Matisse <dnbh7461@EB-L-OR1031401.ad-subs.w2k.francetelecom.fr>
2024-12-01 17:05:42 +0100

    day 1

--- 2024/1/1.py
+++ 2024/1/1.py
@@ -1,10 +1,8 @@
 d = open("input.txt", "r").read().split("\n")
-l, r, s = [], [], 0
+l, r = [], []
 for e in d:
     v = list(map(int, e.split()))
     l.append(v[0])
     r.append(v[-1])
 sl, sr = sorted(l), sorted(r)
-for i in range(len(sl)):
-    s += abs(sl[i] - sr[i])
-print(s)
+print(sum(abs(sl[i] - sr[i]) for i in range(len(sl))))
--- 2024/1/2.py
+++ 2024/1/2.py
@@ -1,14 +1,7 @@
 d = open("input.txt", "r").read().split("\n")
-l, r, s = [], [], 0
+l, r = [], []
 for e in d:
     v = list(map(int, e.split()))
     l.append(v[0])
     r.append(v[-1])
-for el in l:
-    c = 0
-    for er in r:
-        if el == er:
-            c += 1
-    print(c, r.count(el))
-    s += el * c
-print(s)
+print(sum(r.count(e) * e for e in l))
