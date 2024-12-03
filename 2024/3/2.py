ca2ceb3744ece03d6cac8932c8c8e4d5c3667447
Matisse BABONNEAU <matisse.babonneau.ext@veolia.com>
2024-12-03 10:19:53 +0100

    day 3

--- 2024/3/2.py
+++ 2024/3/2.py
@@ -3,8 +3,6 @@ import re
 l, m = open("input.txt", "r").read().replace("\n", ""), []
 m.extend(
     int(x) * int(y)
-    for x, y in re.findall(
-        r"mul\((\d+),(\d+)\)", re.sub(r"don't\(\).*?do\(\)", "", l, flags=re.DOTALL)
-    )
+    for x, y in re.findall(r"mul\((\d+),(\d+)\)", re.sub(r"don't\(\).*?do\(\)", "", l))
 )
 print(sum(m))
