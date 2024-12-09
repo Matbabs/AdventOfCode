0fb7c334649f2bb2d4cc4eeadcd3e8769cfdd6f3
Matisse BABONNEAU <matisse.babonneau.ext@veolia.com>
2024-12-09 14:35:28 +0100

    day 9

--- 2024/9/2.py
+++ 2024/9/2.py
@@ -1,18 +1,23 @@
 import re
 
-d, m, _id = re.findall(r"\d", open("input.txt", "r").read()), [], 0
+d, m, _id, s = re.findall(r"\d", open("input.txt", "r").read()), [], 0, {}
 for i in range(len(d)):
     for j in range(int(d[i])):
         m.append(_id if i % 2 == 0 else -1)
+    s[_id] = int(d[i])
     if i % 2 == 0:
         _id += 1
 _id -= 1
 while _id >= 0:
-    s = m.count(_id)
     fp = m.index(_id)
     for i in range(len(m)):
-        if all(b == -1 for b in m[i : i + s]) and len(m[i : i + s]) == s and i <= fp:
-            for j in range(s):
+        if (
+            m[i] == -1
+            and all(b == -1 for b in m[i : i + s[_id]])
+            and len(m[i : i + s[_id]]) == s[_id]
+            and i <= fp
+        ):
+            for j in range(s[_id]):
                 m[i + j] = _id
                 m[fp + j] = -1
             break
