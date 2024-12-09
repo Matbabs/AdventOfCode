a9042d576f312b9b128cdc86833c5f33542d4ed3
Matisse BABONNEAU <matisse.babonneau.ext@veolia.com>
2024-12-09 08:43:02 +0100

    day 8

---
+++ 2024/8/1.py
@@ -0,0 +1,18 @@
+d, s = open("input.txt", "r").read().split("\n"), set()
+v = lambda x, y: x >= 0 and x < len(d[0]) and y >= 0 and y < len(d)
+p = lambda x, y: s.add(f"{x},{y}")
+for y in range(len(d)):
+    for x in range(len(d[0])):
+        if d[y][x] != ".":
+            a = d[y][x]
+            for y2 in range(len(d)):
+                for x2 in range(len(d[0])):
+                    if x != x2 and y != y2 and a == d[y2][x2]:
+                        dx, dy = x - x2, y - y2
+                        nx1, ny1 = x + dx, y + dy
+                        nx2, ny2 = x2 - dx, y2 - dy
+                        if v(nx1, ny1):
+                            p(nx1, ny1)
+                        if v(nx2, ny2):
+                            p(nx2, ny2)
+print(len(s))
