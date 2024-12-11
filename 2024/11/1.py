96f4c2a4f804027a288f3271b49134965613995f
Matisse BABONNEAU <matisse.babonneau.ext@veolia.com>
2024-12-11 09:43:39 +0100

    day 11

--- 2024/11/1.py
+++ 2024/11/1.py
@@ -1,18 +1,20 @@
+def p(r):
+    sr = str(r)
+    lsr = len(sr)
+    if r == 0:
+        return [1]
+    elif lsr % 2 == 0:
+        return [int(sr[: lsr // 2]), int(sr[lsr // 2 :])]
+    else:
+        return [r * 2024]
+
+
 def s(t):
-    nt = []
     for r in t:
-        sr = str(r)
-        lsr = len(sr)
-        if r == 0:
-            nt.append(1)
-        elif lsr % 2 == 0:
-            nt.extend([int(sr[: lsr // 2]), int(sr[lsr // 2 :])])
-        else:
-            nt.append(r * 2024)
-    return nt
+        yield from p(r)
 
 
 d = list(map(int, open("input.txt", "r").read().split()))
 for i in range(25):
     d = s(d)
-print(len(d))
+print(sum(1 for _ in d))
